32f149a02b2df0fe0de6c198a8860e50f24a20b3
Ubuntu <ubuntu@fido.attlocal.net>
2023-12-11 01:53:54 +0000

    added frontend pull

--- github_puller.sh
+++ github_puller.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 password=$(cat "/home/ubuntu/github_token.txt")
 git -C /home/ubuntu/fido/backend pull https://brianebrahimi:$password@github.com/brianebrahimi/fido_backend 
+git -C /home/ubuntu/fido/fido_frontend/fido_frontend pull https://brianebrahimi:$password@github.com/brianebrahimi/fido_frontend 
 sudo systemctl restart nginx
